fbc848f2e866de0a228c4ab5cde6e628ec6897da
Ch0min <marklundgaard7@gmail.com>
2021-10-05 12:31:04 +0200

    ud

--- Tasks/src/TaskThree/Customer.java
+++ Tasks/src/TaskThree/Customer.java
@@ -1,5 +1,40 @@
 package TaskThree;
 
+    // 3.a
 public class Customer {
-    
-}
+    private String firstName;
+    private String lastName;
+    private String username;
+    private int id;
+
+    // 3.b
+    Customer(String firstName, String lastName, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
+        @Override
+        public String toString() {
+            // Making a newline for each customer
+            String newLine = System.getProperty("line.separator");
+
+            return newLine +"First name: " +firstName+ newLine +"- Last name: " +lastName+ newLine +"- User's username: " +username;
+        }
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+    }
--- Tasks/src/TaskThree/Main.java
+++ Tasks/src/TaskThree/Main.java
@@ -1,9 +1,34 @@
 package TaskThree;
 
+import java.util.ArrayList;
+
 public class Main {
+    // 3.c
+    public static ArrayList<Customer> customers = new ArrayList<>();
 
     public static void main(String[] args) {
+        Customer c1 = new Customer("Mark", "Lundgaard", "Chomin");
+        Customer c2 = new Customer("Cecilie", "Nielsen", "CN");
+        Customer c3 = new Customer("Nick", "Jensen", "NickJ");
+        Customer c4 = new Customer("Tarzan", "Orange", "Tarzo");
+        Customer c5 = new Customer("Susanne", "Nygaard", "SusNy");
+        Customer c6 = new Customer("Brian", "Svend", "BrianS");
 
+        customers.add(c1);
+        customers.add(c2);
+        customers.add(c3);
+        customers.add(c4);
+        customers.add(c5);
+        customers.add(c6);
+        printCustomers();
+
+    }
+    // 3.d
+    public static void printCustomers() {
+        for (Customer c : customers) {
+            System.out.println(c);
+
+        }
     }
 }
 
